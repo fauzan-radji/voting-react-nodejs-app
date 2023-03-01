c7b6354b1ae77115a87f965f22a1ffc5b090ad6c
fauzan-radji <triputrafauzan@gmail.com>
2023-03-01 22:55:58 +0800

    hello world

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
+
 export default function App() {
+  useEffect(() => {
+    document.documentElement.classList.add("dark");
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1 className="text-4xl">Hello World</h1>
     </div>
   );
 }
